bce5c382652af015f2a8a692f94c8b1bcea0d8b8
WDsagor <sagarwpress@gmail.com>
2026-08-07 23:42:04 +0600

    update modal issue solved

--- src/components/modals/TicketModal.jsx
+++ src/components/modals/TicketModal.jsx
@@ -159,20 +159,12 @@ const TicketModal = ({
 
     if (initialData) {
       reset({
-        pnrCode: initialData?.pnrCode,
-        ticketType: initialData?.ticketType,
-        issueDate: formatDateForInput(initialData?.issueDate),
-        passengerName: initialData?.passengerName,
-        route: initialData?.route,
-        travelDate: formatDateForInput(initialData?.travelDate),
-        totalPax: initialData?.totalPax,
-        issuedById: initialData?.issuedById,
-        clientId: initialData?.clientId,
-        airline: initialData?.airline,
-        netCost: Number(initialData?.netCost),
-        clientPrice: Number(initialData?.clientPrice),
-        serviceCharge: Number(initialData?.serviceCharge),
-        status: initialData?.status,
+        ...initialData,
+        issueDate: formatDateForInput(initialData.issueDate),
+        travelDate: formatDateForInput(initialData.travelDate),
+        serviceCharge: initialData?.serviceCharge || 0,
+        netCost: initialData?.netCost || 0,
+        clientPrice: initialData?.clientPrice || 0,
       });
     } else {
       reset({
